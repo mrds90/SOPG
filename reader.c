a85cbf9fbc2823d09598525d9a46c4aeba419b93
Marcos Dominguez <mrds0690@gmail.com>
2021-11-21 18:16:24 -0300

    feat: use mknod to write files

--- reader.c
+++ reader.c
@@ -18,12 +18,21 @@ int main(void) {
     char input_buffer[BUFFER_SIZE];
     char header[HEADER_SIZE];
     int32_t bytes_read, return_code, fd;
-    FILE *fd_sign = fopen(SIGN_FILE, "w");
-    FILE *fd_log = fopen(LOG_FILE, "w");
+    int32_t fd_sign;
+    int32_t fd_log;
 
     /* Create named fifo. -1 means already exists so no action if already exists */
     if ((return_code = mknod(FIFO_NAME, S_IFIFO | 0666, 0)) < -1) {
         printf("Error creating named fifo: %d\n", return_code);
+        exit(1);
+    }
+    printf("got a writer\n");
+    if ((return_code = mknod(SIGN_FILE, S_IFREG | 0666, 0)) < -1) {
+        printf("Error creating named sign file: %d\n", return_code);
+        exit(1);
+    }
+    if ((return_code = mknod(LOG_FILE, S_IFREG | 0666, 0)) < -1) {
+        printf("Error creating named data file: %d\n", return_code);
         exit(1);
     }
     /* Open named fifo. Blocks until other process opens it */
@@ -32,8 +41,16 @@ int main(void) {
         printf("Error opening named fifo file: %d\n", fd);
         exit(1);
     }
-
-    printf("got a writer\n");
+    
+    if ((fd_sign = open(SIGN_FILE, O_WRONLY)) < 0) {
+        printf("Error opening sign file: %d\n", fd);
+        exit(1);
+    }
+    
+    if ((fd_log = open(LOG_FILE, O_WRONLY)) < 0) {
+        printf("Error opening log file: %d\n", fd);
+        exit(1);
+    }
 
     do {
         /* read data into local buffer */
@@ -47,18 +64,28 @@ int main(void) {
 
             snprintf(header, HEADER_SIZE, "%s", input_buffer);
             if (strcmp(header, "SIGN:") == 0) {
-                fprintf(fd_sign, "%s\n", &input_buffer[HEADER_SIZE - 1]);
-                printf("Sign.txt updated with \"%s\"\n", &input_buffer[HEADER_SIZE - 1]);
+                if(write(fd_sign, &input_buffer[HEADER_SIZE - 1], bytes_read - HEADER_SIZE + 1) < 0) {
+                    perror("write");
+                }
+                else {
+                    write(fd_sign, "\n", 1);
+                    printf("Sign.txt updated with: \"%s\"\n", &input_buffer[HEADER_SIZE - 1]);
+                }
             }
             else if (strcmp(header, "DATA:") == 0) {
-                fprintf(fd_log, "%s\n", &input_buffer[HEADER_SIZE - 1]);
-                printf("Log.txt updated with \"%s\"\n", &input_buffer[HEADER_SIZE - 1]);
+                if(write(fd_log, &input_buffer[HEADER_SIZE - 1], bytes_read - HEADER_SIZE + 1)<0) {
+                    perror("write");
+                }
+                else {
+                    write(fd_log, "\n", 1);
+                    printf("Log.txt updated with: \"%s\"\n", &input_buffer[HEADER_SIZE - 1]);
+                }
             }
         }
     }
     while (bytes_read > 0);
-    fclose(fd_sign);
-    fclose(fd_log);
+    // fclose(fd_sign);
+    // fclose(fd_log);
 
     return 0;
 }
